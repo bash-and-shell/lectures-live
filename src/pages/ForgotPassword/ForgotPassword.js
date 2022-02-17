f19654bc3ecae917bc7817498b9bd344f14c95d6
Zach Thomas <sc18zt@leeds.ac.uk>
2022-02-17 14:40:58 +0000

    Forgot password setup

---
+++ src/pages/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import isEmail from 'validator/lib/isEmail';
+
+const theme = createTheme();
+
+const ForgotPassword = () => {
+
+  const [emailValid, setEmailValid] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    setEmailValid(isEmail(data.get('email')))
+
+    if (emailValid) {
+      const response = await fetch('http://localhost:5005/api/v1/users/user', {
+        method: 'PUT',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: data.get('email'),
+          password: data.get('password'),
+          type: value.toLowerCase()
+        }),
+      })
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Forgot your password?
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography component="h5" variant="subtitle1">
+                  Please enter your email address
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  error={emailValid === false}
+                  helperText={emailValid===false ? "Please enter a valid email address." : null}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Forgot Password
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default ForgotPassword;
