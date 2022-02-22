f110c84b421eac93e1de23376de67035b0d8dd1c
Zach Thomas <sc18zt@leeds.ac.uk>
2022-02-22 17:05:52 +0000

    Added auth middleware to lecture routes

--- backend/api/lectures.routes.js
+++ backend/api/lectures.routes.js
@@ -1,16 +1,17 @@
 import express from 'express';
+import checkAuth from '../middleware/checkAuth.js'
 import LecturesController from './lectures.controller.js';
 const router = express.Router();
 
 router.route('/')
-.get(LecturesController.getLectures)
+.get(checkAuth, LecturesController.getLectures)
 
-router.route('/register')
-.post(LecturesController.createLecture)
+router.route('/createLecture')
+.post(checkAuth, LecturesController.createLecture)
 
 router.route('/lecture')
-.get(LecturesController.getLecture)
-.put(LecturesController.updateLecture)
-.delete(LecturesController.deleteLecture)
+.get(checkAuth, LecturesController.getLecture)
+.put(checkAuth, LecturesController.updateLecture)
+.delete(checkAuth, LecturesController.deleteLecture)
 
 export default router;
