eff553d4a61189deda30cec175880d3f365ba833
Zach Thomas <sc18zt@leeds.ac.uk>
2022-02-22 13:33:58 +0000

    Adding password hashing with bcrypt

--- backend/api/user.controller.js
+++ backend/api/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 export default class UsersController {
   static async getUsers(req, res, next) {
     try {
@@ -12,34 +13,49 @@ export default class UsersController {
   }
 
   static async getUser(req, res, next) {
-    try {
-      const user = await User.findOne({
+      let fetchedUser
+      // const xtoken = req.headers['x-access-token'];
+
+      // try{
+      //   const decoded = jwt.verify(xtoken, process.env.JWT_SECRET);
+      //   const email = decoded.email;
+      // }
+      // catch (err) {
+        
+      // }
+      User.findOne({
         email: req.body.email,
-        password: req.body.password,
+      }).then(user => {
+        if(!user) {
+          return res.status(401).json({ success: false, user: false })
+        }
+
+        fetchedUser=user
+        return bcrypt.compare(req.body.password, user.password)
+      }).then(result => {
+        if(!result) {
+          return res.status(401).json({ success: false, user: false, msg: 'Incorrect Password' })
+        }
       })
 
-      if (user) {
-        const token = jwt.sign({
-          email: user.email,
-          type: user.type,
-        }, process.env.JWT_SECRET)
+      const token = jwt.sign(
+        { email: user.email, type: user.type }, 
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+      )
 
-        return res.status(200).json({ success: true, user: token })
-      }
-      else {
-        return res.status(401).json({ success: false, user: false })
-      }
-    } catch (err) {
-      return res.status(401).json({ success: false, user: false, msg: err.message })
-    }
+      return res.status(200).json({ success: true, user: token, expiresIn: 3600 })
   }
 
   static async createUser(req, res, next) {
     try {
-      const user = await User.create({
-        email: req.body.email,
-        password: req.body.password,
-        type: req.body.type,
+      bcrypt.hash(req.body.password, 10).then(hash => {
+        const user = await User.create({
+          email: req.body.email,
+          password: hash,
+          type: req.body.type,
+        })
+  
       })
 
       console.log(`User created: ${user}`)
