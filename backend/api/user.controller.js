ab32f43319d38fa985c4d977aef462293790a186
Zach Thomas <sc18zt@leeds.ac.uk>
2022-02-22 16:02:15 +0000

    Changed to use async await

--- backend/api/user.controller.js
+++ backend/api/user.controller.js
@@ -13,52 +13,50 @@ export default class UsersController {
   }
 
   static async getUser(req, res, next) {
-      let fetchedUser
-      // const xtoken = req.headers['x-access-token'];
+    try {
+      const user = await User.findOne({email: req.body.email})
+      if (!user) {
+        return res.status(401).json({ success: false, user: false })
+      }
 
-      // try{
-      //   const decoded = jwt.verify(xtoken, process.env.JWT_SECRET);
-      //   const email = decoded.email;
-      // }
-      // catch (err) {
-        
-      // }
-      User.findOne({
-        email: req.body.email,
-      }).then(user => {
-        if(!user) {
-          return res.status(401).json({ success: false, user: false })
-        }
-
-        fetchedUser=user
-        return bcrypt.compare(req.body.password, user.password)
-      }).then(result => {
-        if(!result) {
-          return res.status(401).json({ success: false, user: false, msg: 'Incorrect Password' })
-        }
-      })
+      const validPassword = await bcrypt.compare(req.body.password, user.password)
+      if (!validPassword) {
+        return res.status(401).json({ success: false, user: false, msg: 'Incorrect Password' })
+      }
 
       const token = jwt.sign(
-        { email: user.email, type: user.type }, 
+        { email: user.email, type: user.type },
         process.env.JWT_SECRET,
         { expiresIn: "1h" }
       )
+      console.log(user)
 
-      return res.status(200).json({ success: true, user: token, expiresIn: 3600 })
+      return res.status(200).json({ success: true, token: token, expiresIn: 3600, userId: user._id })
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ success: false, msg: err.message })
+    }
   }
 
   static async createUser(req, res, next) {
     try {
-      bcrypt.hash(req.body.password, 10).then(hash => {
-        const user = User.create({
-          email: req.body.email,
-          password: hash,
-          type: req.body.type,
-        })
+      const hash = await bcrypt.hash(req.body.password, 10);
+
+      const userExist = await User.findOne({ email: req.body.email })
+
+      if(userExist) {
+        return res.status(401).json({ success: false, msg: 'User already exists'})
+      }
+
+      const user = await User.create({
+        email: req.body.email,
+        password: hash,
+        type: req.body.type,
       })
 
       console.log(`User created: ${user}`)
-      return res.json({ success: true })
+      return res.status(201).json({ success: true })
     }
     catch (err) {
       //more error handling
