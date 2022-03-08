ae99415b92e1a52ddceb3124cd948e49b8116512
Zach Thomas <sc18zt@leeds.ac.uk>
2022-03-08 15:17:15 +0000

    Fonts & component routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import UserRoute from './components/UserRoute'
@@ -6,6 +10,8 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import ForgotPassword from './pages/ForgotPassword'
 import AccountPage from './pages/AccountPage'
+import JoinSession from './pages/student/JoinSession'
+import CreateSession from './pages/teacher/CreateSession'
 import useCheckUser from './hooks/useCheckUser'
 
 const App = () => {
@@ -18,7 +24,12 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/account" element={<UserRoute><AccountPage/></UserRoute>} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/join" element={<JoinSession />} />
+        <Route path="/create" element={<CreateSession />} />
+        {/* <Route path='/account/:username' element={<UserRoute><AccountPage/></UserRoute>} /> */}
+        {/* <Route path="/join" element={<UserRoute><JoinSession/></UserRoute>} /> */}
+        {/* <Route path="/create" element={<UserRoute><CreateSession/></UserRoute>} /> */}
       </Routes>
     </Router>
   </React.Fragment>
