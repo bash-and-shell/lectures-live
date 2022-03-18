23063189a373ba1e678418b7b57dbab38ab28f86
Zach Thomas <sc18zt@leeds.ac.uk>
2022-03-18 15:31:39 +0000

    Change lecture model to use user_id insted of email

--- backend/models/lecture.model.js
+++ backend/models/lecture.model.js
@@ -2,21 +2,23 @@ import mongoose from 'mongoose';
 import isEmail from 'validator/lib/isEmail.js';
 
 const responseSchema = new mongoose.Schema({
-  email: {type: String, required: true, validate: [isEmail, 'invalid email']},
+  user_id: {type: mongoose.Types.ObjectId, required: true},
   response_type: { type: String, enum: ['feeling', 'question'], required: true },
+  response : { type: String, required: true},
   time: { type: Date, required: true }
 })
 
 const lectureSchema = new mongoose.Schema({
-  user: {type: String, required: true, validate: [isEmail, 'invalid email']},
+  user_id: {type: mongoose.Types.ObjectId, required: true},
   title: { type: 'string', required: true },
+  time: { type: Date, required: true },
   responses: { type: [responseSchema] }
 },
   { collection: 'lectures' }
 )
 
 //make user and title unique pair
-lectureSchema.index({user: 1, title: 1}, {unique: true})
+lectureSchema.index({user_id: 1, title: 1}, {unique: true})
 
 const model = mongoose.model('Lecture', lectureSchema)
 
