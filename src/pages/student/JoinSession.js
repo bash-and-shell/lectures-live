37d1342e04e0d31d769a8e1a518d5c77b7d2b827
Zach Thomas <sc18zt@leeds.ac.uk>
2022-03-21 14:21:53 +0000

    Updated join session to nav to correct place, removed unused imports

--- src/pages/student/JoinSession.js
+++ src/pages/student/JoinSession.js
@@ -1,22 +1,15 @@
 import "../scss/JoinSession.scss"
 import React, { useEffect, useState, useContext } from 'react';
 import { UserContext } from '../../context/UserContext'
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Autocomplete from '@mui/material/Autocomplete';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link as RouterLink } from 'react-router-dom';
 
 
 const theme = createTheme();
@@ -27,7 +20,9 @@ const JoinSession = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const data = new FormData(e.currentTarget);
 
+    navigate(`/session/${data.get('code')}`)
   }
 
   return (
