fe165b8e0c30917dee4991f64eb9081830f725e6
Zach Thomas <sc18zt@leeds.ac.uk>
2022-04-06 15:06:54 +0100

    Pass through props to backbutton

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -4,11 +4,11 @@ import { IconButton } from '@mui/material'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import './scss/BackButton.scss'
 
-const BackButton = () => {
+const BackButton = (props) => {
   const navigate = useNavigate();
 
   return (
-    <IconButton  className="arrow-back"  onClick={()=>{navigate(-1)}} >
+    <IconButton {...props} className="arrow-back"  onClick={()=>{navigate(-1)}} >
       <ArrowBackIosNewIcon sx={{color: 'white'}}/>
     </IconButton>
   )
