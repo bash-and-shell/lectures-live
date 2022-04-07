2662dbb678df7c6ec34a1fd3f7e3dc498ca8f17e
Zach Thomas <sc18zt@leeds.ac.uk>
2022-04-07 11:58:51 +0100

    Add TextFields for updating and SpeedDial

--- src/pages/student/StudentAccountPage.js
+++ src/pages/student/StudentAccountPage.js
@@ -1,66 +1,184 @@
 import '../scss/AccountPage.scss'
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { UserContext } from '../../context/UserContext'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom'
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Autocomplete from '@mui/material/Autocomplete';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Container from '@mui/material/Container';
+import { useNavigate, useParams } from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link as RouterLink } from 'react-router-dom';
-import PageHeader from '../../components/PageHeader'
 import { deepPurple } from '@mui/material/colors';
+import BackButton from '../../components/BackButton';
+import CoPresentIcon from '@mui/icons-material/CoPresent';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useAuth } from '../../hooks'
+import isEmail from 'validator/lib/isEmail';
+import isStrongPassword from 'validator/lib/isStrongPassword';
 
+import {
+  Alert,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  SpeedDial,
+  SpeedDialIcon,
+  SpeedDialAction,
+  TextField,
+  Typography
+} from '@mui/material'
 
 const theme = createTheme();
 
 const AccountPage = () => {
   const { user } = useContext(UserContext)
   const navigate = useNavigate();
+  const { id } = useParams()
   const [emailValid, setEmailValid] = useState(null)
   const [usernameValid, setUsernameValid] = useState(null)
-
-  const handleChangeEmail = (e) => {
+  const [confirmValid, setConfirmValid] = useState(null)
+  const [strongPassword, setStrongPassword] = useState(null)
+  const { updateUser, logoutUser, error, setError } = useAuth()
+  const [ successMessage, setSuccessMessage ] = useState(null)
+  const handleForm = (e) => {
     e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    if(!data.get('email') && !data.get('username') && !data.get('password')) {
+      setError("No data entered")
+      return
+    }
+    let updateData = {}
+    if(data.get('email')) {
+      setEmailValid(isEmail(data.get('email')))
+      if(!emailValid)
+        return
+      updateData.email = data.get('email')
+    }
+
+    if(data.get('username')) {
+      updateData.username = data.get('username')
+    }
+
+    if(data.get('password') && data.get('confirmPassword')) {
+      if(data.get('password') !== data.get('confirmPassword')) {
+        setConfirmValid(false)
+        return
+      }
+      
+      setStrongPassword(isStrongPassword(data.get('password')))
+      if(!strongPassword)
+        return
+      
+      updateData.password = data.get('password')
+    }
+    
+    updateUser(updateData)
+
+    if(!error) {
+      setSuccessMessage("Account updated successfully")
+    }
   }
 
-  const handleChangeUsername = (e) => {
-    e.preventDefault();
+  const passwordRequirementsText = () => {
+    return <p>
+      Please enter a password with at least:
+      <li>8 characters</li>
+      <li>1 uppercase</li>
+      <li>1 lowercase</li>
+      <li>1 special character</li>
+    </p>
   }
+
+  const speedDialActions = [
+    { icon: <CoPresentIcon />, name: 'Join Session', onClick: () => navigate('/join') },
+    { icon: <LogoutIcon />, name: 'Logout', onClick: () => logoutUser() },
+  ]
 
   return (
     <ThemeProvider theme={theme}>
       {/* <PageHeader /> */}
-      <Container disableGutters>
-        <Grid container spacing={2}>
-          <Grid backgroundColor="primary.main" xs={12} sx={{ height: '30vh' }}>
-          </Grid>
-          <Grid container sx={{ justifyContent: 'center' }}>
-            <Avatar id="user-icon" sx={{ bgcolor: deepPurple[500], height: 60, width: 60, alignSelf: 'center' }}></Avatar>
-            <Grid item xs={12} sx={{ textAlign: 'center' }}>
-              <Typography variant="h5" component="div" id='heading' >
-                *username*
-              </Typography>
-              <Typography variant="body1" component="div">
-                Update your account info here
-              </Typography>
-            </Grid>
-          </Grid>
+
+      <Grid container>
+        <Grid item backgroundColor="primary.main" xs={12} sx={{ height: '5rem' }}>
+          <BackButton sx={{ paddingLeft: '2rem' }} />
         </Grid>
-        {/* <Box backgroundColor="primary.main"
+        <Grid container item sx={{ justifyContent: 'center' }}>
+          <Avatar id="user-icon" sx={{ bgcolor: deepPurple[500], height: 60, width: 60, alignSelf: 'center', transform: 'translateY(-25px)' }}></Avatar>
+          <Grid item xs={12} sx={{ textAlign: 'center' }}>
+            <Typography variant="h5" component="div" >
+              {id}
+            </Typography>
+            <Typography variant="body1" component="div">
+              Update your account info here
+            </Typography>
+          </Grid>
+          <Container maxWidth='xs'>
+              <Box component="form" onSubmit={handleForm} noValidate sx={{ mt: 1 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  error={usernameValid === false}
+                  helperText={usernameValid === false ? "This username already exists" : null}
+                />
+                </Grid>
+                <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  error={usernameValid === false}
+                  helperText={usernameValid === false ? "This username already exists" : null}
+                />
+                </Grid>
+                <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="password"
+                  label="New Password"
+                  name="password"
+                  error={strongPassword === false}
+                  helperText={strongPassword === false ? passwordRequirementsText() : null}
+                />
+                </Grid>
+                <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="confirmPassword"
+                  label="Confirm Password"
+                  name="confirmPassword"
+                  error={confirmValid === false}
+                  helperText={confirmValid === false ? "Please ensure passwords match" : null}
+                />
+                </Grid>
+                {error &&
+                <Grid item xs={12}>
+                  <Alert severity="error">{error}</Alert>
+                </Grid>}
+                {successMessage &&
+                <Grid item xs={12}>
+                  <Alert severity="success">{successMessage}</Alert>
+                </Grid>}
+                <Grid item xs={12}>
+                <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+            >
+              Update Info
+            </Button>
+                </Grid>
+
+               
+                            </Grid>
+              </Box>
+          </Container>
+
+        </Grid>
+      </Grid>
+      {/* <Box backgroundColor="primary.main"
           sx={{
             height: '30vh',
             // flexGrow: 4,
@@ -127,11 +245,23 @@ const AccountPage = () => {
 
           }
         </Grid>
+        onClick={() => navigate('/join')}
         </Box> */}
-        <Fab variant='extended' color='primary' id='fab-join' onClick={() => navigate('/join')}>
-          Join Session
-        </Fab>
-      </Container>
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'fixed', bottom: '1rem', right: '1rem' }}
+        icon={<SpeedDialIcon />}
+      >
+        {speedDialActions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            tooltipOpen
+            onClick={action.onClick}
+          />
+        ))}
+      </SpeedDial>
     </ThemeProvider>
   )
 }
